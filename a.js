383bcc2d9767ce2c1bdfd8f0911981272ea96be5
Thomas Bhattarai <thomasbhattarai@gmail.com>
2025-04-19 21:59:56 +0545

    1

--- a.js
+++ a.js
@@ -1,1 +1,7 @@
-forget
+let price = [250, 300, 350, 400, 450, 500];
+for (let i = 0; i < price.length; i++) {
+   let discount = 0.1 * price[i];
+   let discountedPrice = price[i] - discount;
+   console.log('the discount price is', discountedPrice);
+
+}
